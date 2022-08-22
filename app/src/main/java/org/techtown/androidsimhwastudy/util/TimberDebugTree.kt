237c7d557307b480d5cdca18e35630e2df6f5fea
murjune <sis9221302@gmail.com>
2022-08-22 19:29:21 +0900

    [feat] Timber 추가

---
+++ app/src/main/java/org/techtown/androidsimhwastudy/util/TimberDebugTree.kt
@@ -0,0 +1,10 @@
+package org.techtown.androidsimhwastudy.util
+
+import timber.log.Timber
+
+class TimberDebugTree : Timber.DebugTree() {
+
+    override fun createStackElementTag(element: StackTraceElement): String? {
+        return "${element.fileName} : ${element.lineNumber} - ${element.methodName}"
+    }
+}
